db748a0c064a94dba2cc2233516efdcb64a590f2
tammy228 <saintpaulstammy@gmail.com>
2026-05-02 00:24:24 +0800

    Add 206.

---
+++ linked_list/206.Reverse Linked List(E).py
@@ -0,0 +1,28 @@
+"""
+My Solution
+
+use prev, curr, next to step by step revert the linked list
+
+Time:
+O(N)
+
+Space:
+O(1)
+"""
+# Definition for singly-linked list.
+from typing import Optional
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        prev = None
+        while head:
+            temp = head.next
+            head.next = prev
+            prev = head
+            head = temp
+        return prev
+
+        
